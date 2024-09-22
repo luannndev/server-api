7063b13ca4eef9f991a4a05ef032ff63dd2f7ae3
luannndev <106454269+luannndev@users.noreply.github.com>
2024-09-22 18:00:19 +0200

    Add InvalidConfigurationException class
    Introduce a new exception class named InvalidConfigurationException to handle configuration-related errors. This exception extends RuntimeException and provides a constructor to accept an error message.

---
+++ api-common/src/main/java/dev/luan/common/configuration/exception/InvalidConfigurationException.java
@@ -0,0 +1,8 @@
+package dev.luan.common.configuration.exception;
+
+public class InvalidConfigurationException extends RuntimeException {
+
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}
